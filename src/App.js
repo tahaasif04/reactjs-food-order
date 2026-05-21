73f1ef975de4786e43fe8f7149a581a4bc116ee6
tahaasif04 <taha.asif04@gmail.com>
2026-05-21 11:23:47 +0500

    hhh

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
 
 
